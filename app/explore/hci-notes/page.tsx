9ba462d8f2799bc79dc04580bef69ac6374c86c6
Siddhant Patil <sidomatic11@gmail.com>
2025-03-19 22:39:50 +0530

    hci page

---
+++ app/explore/hci-notes/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/app/components/header";
+
+export default function HCI() {
+  return (
+    <div>
+      <Header isProjectHeader={true} />
+      <div>HCI Notes</div>
+    </div>
+  );
+}
