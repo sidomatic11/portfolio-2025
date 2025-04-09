77d9a88445a2777fdb66357e2031322c5419d61d
Siddhant Patil <sidomatic11@gmail.com>
2025-04-09 16:40:22 +0530

    Header Z

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -6,7 +6,7 @@ export default function Header({ toggleExploreSolve, isExploreVisible, isProject
 
 
 	return (
-		<header className="w-full flex items-center justify-between h-16 bg-white fixed bottom-0 left-0 right-0 shadow-[inset_0_1px_0_0_black] sm:top-0 sm:shadow-[0_1px_0_0_black]">
+		<header className="w-full flex items-center justify-between h-16 bg-white fixed bottom-0 left-0 right-0 z-50 shadow-[inset_0_1px_0_0_black] sm:top-0 sm:shadow-[0_1px_0_0_black]">
 
 			<div className="w-full max-w-5xl mx-auto h-full flex items-center justify-between lg:px-8">
 				<div className="shadow-[inset_-1px_0_0_0_black,-1px_0_0_0_black] h-full w-16 flex items-center justify-center flex-shrink-0">
