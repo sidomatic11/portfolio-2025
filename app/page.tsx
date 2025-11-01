42f98dc52ef3ace046822a0faafe45fbab2a47c4
Siddhant Patil <sidomatic11@gmail.com>
2025-11-01 08:33:39 +0100

    unused component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
 import Header from "./components/header";
 import { useState, useEffect } from "react";
 import ProjectCard from "./components/project-card";
